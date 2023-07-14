ed2a1540aa9c3c82bbec2786285edede833977d9
7185 <7185@users.noreply.github.com>
2023-07-15 00:52:37 +0200

    fix(backend): remove outdated shutdown hook for db

--- backend/src/db/db.service.spec.ts
+++ backend/src/db/db.service.spec.ts
@@ -1,16 +1,10 @@
-import {NestFastifyApplication} from '@nestjs/platform-fastify'
 import {Test, TestingModule} from '@nestjs/testing'
 import {DbService} from './db.service'
 
 describe('DbService', () => {
-  const MockApp = jest.fn<Partial<NestFastifyApplication>, []>(() => ({
-    close: jest.fn()
-  }))
   let service: DbService
-  let app: NestFastifyApplication
 
   beforeEach(async () => {
-    app = MockApp() as NestFastifyApplication
     const module: TestingModule = await Test.createTestingModule({
       providers: [DbService]
     }).compile()
@@ -32,22 +26,6 @@ describe('DbService', () => {
 
       expect(spy).toBeCalledTimes(1)
       expect(service.$connect).toBeCalledTimes(1)
-      spy.mockRestore()
-    })
-  })
-
-  describe('enableShutdownHooks', () => {
-    it('should call $on and successfully close the app', async () => {
-      const spy = jest
-        .spyOn(DbService.prototype, '$on')
-        .mockImplementation(async (_, cb: (_: () => Promise<void>) => void) =>
-          cb(() => Promise.resolve())
-        )
-
-      await service.enableShutdownHooks(app)
-
-      expect(spy).toBeCalledTimes(1)
-      expect(app.close).toBeCalledTimes(1)
       spy.mockRestore()
     })
   })
--- backend/src/db/db.service.ts
+++ backend/src/db/db.service.ts
@@ -1,16 +1,9 @@
 import {Injectable, OnModuleInit} from '@nestjs/common'
-import {NestFastifyApplication} from '@nestjs/platform-fastify'
 import {PrismaClient} from '@prisma/client'
 
 @Injectable()
 export class DbService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect()
-  }
-
-  async enableShutdownHooks(app: NestFastifyApplication) {
-    this.$on('beforeExit', async () => {
-      await app.close()
-    })
   }
 }
