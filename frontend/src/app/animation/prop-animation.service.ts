392b6974ae79ac4f6916656dd0c38e3c940f38cf
7185 <7185@users.noreply.github.com>
2023-12-18 19:07:59 +0100

    fix: improve props movement code and avoid drifts

--- frontend/src/app/animation/prop-animation.service.ts
+++ frontend/src/app/animation/prop-animation.service.ts
@@ -17,52 +17,41 @@ export class PropAnimationService {
     if (moveData.waiting > 0) {
       // We're still waiting
       moveData.waiting -= delta
-    } else if (moveData.completion < 1) {
-      // If the move is in progress, update the item's position
-      const moveFactor = moveData.direction * (delta / moveData.time)
-      item.position.x += moveData.distance.x * moveFactor
-      item.position.y += moveData.distance.y * moveFactor
-      item.position.z += moveData.distance.z * moveFactor
-      moveData.completion += delta / moveData.time
+      return
+    }
+    // Update position
+    // If reset is set, we have to do the move twice so we force the direction here
+    // in order to keep the state (if direction is -1, we're on the second move)
+    const moveFactor =
+      (moveData.reset ? 1 : moveData.direction) * (delta / moveData.time)
+    item.position.x += moveData.distance.x * moveFactor
+    item.position.y += moveData.distance.y * moveFactor
+    item.position.z += moveData.distance.z * moveFactor
+    moveData.completion += delta / moveData.time
 
-      // Check if the item has reached its destination (completion = 1)
-      if (moveData.completion >= 1) {
-        if (moveData.loop) {
-          // If looping is enabled, reset the completion and change the direction for the way back
-          moveData.direction *= -1
-          moveData.completion = 0
-          // Add a wait time before starting the way back
-          moveData.waiting = moveData.wait
-        } else {
-          // If looping is not enabled, set the completion to 1 to indicate the move is finished
-          moveData.completion = 1
-        }
-        if (moveData.reset && moveData.direction === -1) {
-          // Reset the item's position to the original position
-          item.position.copy(
-            new Vector3().add(moveData.orig).sub(item.parent.parent.position)
-          )
-          moveData.direction = 1
-        }
-      }
-    } else if (moveData.direction === -1) {
-      // If the way back is done and direction is -1 (indicating the way back)
-      if (moveData.loop) {
-        // If looping is enabled, reset the direction and completion for the next forward movement
-        moveData.direction *= -1
-        moveData.completion = 0
-        // Add a wait time before starting the next forward movement
-        moveData.waiting = moveData.wait
-        // The item should be back to it's initial position
+    // Check if the item has reached its destination (completion = 1)
+    if (moveData.completion >= 1) {
+      moveData.completion = 0
+      if (moveData.reset) {
+        // Reset the position to the original position
         item.position.copy(
           new Vector3().add(moveData.orig).sub(item.parent.parent.position)
         )
       }
-    } else {
-      // Way back is starting
-      // Reset the direction and completion for the way back
+
+      if (moveData.direction < 0) {
+        if (!moveData.loop) {
+          // No loop and way back is done, so all is done
+          item.userData.animation.move = undefined
+          return
+        }
+        // Move is complete for this loop so we can force the initial position back to avoid drifting issues
+        item.position.copy(
+          new Vector3().add(moveData.orig).sub(item.parent.parent.position)
+        )
+      }
+      // Switch direction
       moveData.direction *= -1
-      moveData.completion = 0
       // Add a wait time before starting the way back
       moveData.waiting = moveData.wait
     }
@@ -76,40 +65,40 @@ export class PropAnimationService {
     if (rotateData.waiting > 0) {
       // We're still waiting
       rotateData.waiting -= delta
-    } else {
-      // Update rotation
-      item.rotateOnAxis(
-        Y_AXIS,
-        rotateData.speed.y * RPM * delta * rotateData.direction
-      )
-      item.rotateOnAxis(
-        Z_AXIS,
-        rotateData.speed.z * RPM * delta * rotateData.direction
-      )
-      item.rotateOnAxis(
-        X_AXIS,
-        rotateData.speed.x * RPM * delta * rotateData.direction
-      )
-      // Update the rotation completion based on the provided time
-      rotateData.completion += delta / rotateData.time
-      // Check if rotation has completed (completion >= 1)
-      if (rotateData.completion >= 1) {
-        rotateData.completion = 0
-        if (rotateData.reset) {
-          // Reset the rotation to the original rotation
-          item.rotation.copy(rotateData.orig)
-        } else {
-          if (!rotateData.loop && rotateData.direction < 0) {
-            // No loop and way back is finished so all is done
-            item.rotation.copy(rotateData.orig)
-            item.userData.animation.rotate = undefined
-            return
-          }
-          // Add a wait time before starting the next rotation
-          rotateData.waiting = rotateData.wait
+      return
+    }
+    // Update rotation
+    item.rotateOnAxis(
+      Y_AXIS,
+      rotateData.speed.y * RPM * delta * rotateData.direction
+    )
+    item.rotateOnAxis(
+      Z_AXIS,
+      rotateData.speed.z * RPM * delta * rotateData.direction
+    )
+    item.rotateOnAxis(
+      X_AXIS,
+      rotateData.speed.x * RPM * delta * rotateData.direction
+    )
+    // Update the rotation completion based on the provided time
+    rotateData.completion += delta / (rotateData.time || 1)
+    // Check if rotation has completed (completion >= 1)
+    if (rotateData.completion >= 1) {
+      rotateData.completion = 0
+      if (rotateData.reset) {
+        // Reset the rotation to the original rotation
+        item.rotation.copy(rotateData.orig)
+      }
+      if (rotateData.time) {
+        // Add a wait time before starting the next rotation
+        rotateData.waiting = rotateData.wait
+        if (rotateData.loop || rotateData.wait) {
           // Reverse the rotation direction for the next rotation
           rotateData.direction *= -1
+          return
         }
+        // No loop nor wait, so the rotation is over
+        item.userData.animation.rotate = undefined
       }
     }
   }
